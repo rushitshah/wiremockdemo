9db63630cd562e21ee5e931f668b66d55bd131ec
GamerX <rushitshah10@gmail.com>
2021-12-27 10:12:55 -0600

    Simple Wiremock Application

--- src/main/java/com/wiremock/wiremockdemo/WiremockdemoApplication.java
+++ src/main/java/com/wiremock/wiremockdemo/WiremockdemoApplication.java
@@ -1,32 +1,17 @@
 package com.wiremock.wiremockdemo;
 
+
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.core.Options;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.contract.wiremock.WireMockSpring;
-import org.springframework.context.annotation.Bean;
 
-import java.io.IOException;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
-@SpringBootApplication
-public class WiremockdemoApplication extends SpringBootServletInitializer {
+public class WiremockdemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(WiremockdemoApplication.class, args);
-  }
 
-  @Bean
-  public Options wireMockOptions() throws IOException {
-
-    final WireMockConfiguration options = WireMockSpring.options();
-    options.port(8080);
-    WireMockServer wireMockServer = new WireMockServer();
+    WireMockServer wireMockServer = new WireMockServer(options().port(8080));
     wireMockServer.start();
 
-    return options;
   }
 
 }
